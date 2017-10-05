b34708d75f2884a3e9ed9d34499e7c4a451164c4
RishadYamnoor <rishad.yamnoor@gmail.com>
2017-10-05 09:13:59 +0530

    changed for loop and color function

--- designs.js
+++ designs.js
@@ -1,48 +1,40 @@
-// Select color input
-// Select size input
+const heightInput = $('#input_height');
+const widthInput = $('#input_width');
+const colorInput = $('#colorPicker');
+
+//makeGrid function that removes previous table and then new grid is built with help of for-loop
+
+makeGrid = () => {
+
+  let heightValue = heightInput.val();
+  let widthValue = widthInput.val();
+
+  let pixelCanvas = $('#pixel_canvas');
+  pixelCanvas.children().remove(); // clears previously built table if any
+
+
+// grid is built
+
+  for (let h = 0; h < heightValue; h++) {
+    pixelCanvas.append("<tr></tr>");
+  }
+  for (let w = 0; w < widthValue; w++) {
+    $('tr').append("<td></td>");
+  }
+}
+
+
+//Changes the background color of the cell, with respective chosen color
+
+$('table').on('click', 'td', function() {
+  $(this).css('backgroundColor', colorInput.val() );
+});
+
 
 // When size is submitted by the user, call makeGrid()
 
-makeGrid = () => {
-  const pixelCanvas = $('#pixel_canvas');
-  const heightValue = $('#input_height').val();
-  const widthValue = $('#input_width').val();
-  pixelCanvas.innerText = ""; //empty table
-
-
-  for (let h = 0; h < heightValue; ++h) {
-    const row = pixelCanvas.insertRow(-1);
-  for (let w = 0; w < widthValue; ++w) {
-      const cell = row.insertCell(-1);
-      cell.onclick = changeColor;
-    }
-  }
-  event.preventDefault(); // stops page from refreshing upon submit
-}
-
-
-changeColor = () => {
-  const color = $('#colorPicker').val();
-  this.style.background = color;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $('#sizePicker').on("submit", function(event){
-//   console.log(`height: ${heightValue} and width: ${widthValue}`);
-//   makeGrid();
-//   event.preventDefault(); // stops page from refreshing upon submit
-// });
-
+form = $('#sizePicker');
+form.submit(function(event) {
+    event.preventDefault(); // stops page from refreshing upon submit
+    makeGrid();
+});
